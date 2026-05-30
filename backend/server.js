5d14841556817c81cff24afbcf25198e95a498df
loganyeh <yehlogan@gmail.com>
2026-05-29 21:06:56 -0400

    prepare backend for deploy

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import Wishlist from "./models/Wishlist.js";
-
-const MONGO_URI = "mongodb+srv://loganyehdev_db_user:3AegTqokMycV0KP0@cluster0.cetpm2m.mongodb.net/?appName=Cluster0";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
